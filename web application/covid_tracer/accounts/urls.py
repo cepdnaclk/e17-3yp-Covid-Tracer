933857491398b804c6cb480f6a176ca073a8811d
E17044-DeannaCoralage <deannatscoralage@gmail.com>
2021-10-01 21:48:30 +0530

    added settings page

--- web application/covid_tracer/accounts/urls.py
+++ web application/covid_tracer/accounts/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('otp', views.otp, name='otp'),
     path('home', views.home, name='home'),
     path('trace', views.trace, name='trace'),
+    path('myaccount', views.myaccount, name='myaccount'),
+    path('forgotpassword', views.forgotpassword, name='forgotpassword'),
     path('logout', views.logout, name='logout'),
-
 ]
