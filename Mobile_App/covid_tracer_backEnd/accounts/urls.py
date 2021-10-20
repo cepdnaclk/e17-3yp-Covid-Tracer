ac797c10039c20504426b5a28bfd8c0bf3505111
Ruchika-Perera <pereraruchika7@gmail.com>
2021-10-21 02:58:07 +0530

    frontEnd login validation

---
+++ Mobile_App/covid_tracer_backEnd/accounts/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('confirm', views.confirm, name='confirm'),
+    path('otp', views.otp, name='otp'),
+    path('home', views.home, name='home'),
+    path('trace', views.trace, name='trace'),
+    #path('myaccount', views.myaccount, name='myaccount'),
+    #path('resetpassword', views.resetpassword, name='resetpassword'),
+    #path('forgotpassword', views.forgotpassword, name='forgotpassword'),
+    path('logout', views.logout, name='logout'),
+]
